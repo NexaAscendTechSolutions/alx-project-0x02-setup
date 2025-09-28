cb99fe725548f7826f2a9a95aee64c88addc6296
nexaascendtechsolutions <admin@nexaascendtechsolutions.co.ke>
2025-09-28 22:59:17 +0300

    fixing errors

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,12 +1,10 @@
-import { CardProps } from "../../interfaces";
+import { type CardProps } from "@/interfaces";
 
 export default function Card({ title, content }: CardProps) {
   return (
     <div className="border rounded-lg shadow-md p-6 bg-white dark:bg-gray-800">
-      <h2 className="text-xl font-semibold mb-2 text-gray-900 dark:text-gray-100">
-        {title}
-      </h2>
-      <p className="text-gray-700 dark:text-gray-300">{content}</p>
+      <h2 className="text-xl font-semibold mb-2">{title}</h2>
+      <p className="text-gray-600 dark:text-gray-300">{content}</p>
     </div>
   );
 }
